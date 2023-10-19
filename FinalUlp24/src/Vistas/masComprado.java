8bc1e94ceb00a11e96451bd4ee997f167ea240d6
Martin7304 <105321420+Martin7304@users.noreply.github.com>
2023-10-19 20:55:43 -0300

    por frcha

--- FinalUlp24/src/Vistas/masComprado.java
+++ FinalUlp24/src/Vistas/masComprado.java
@@ -5,6 +5,11 @@
  */
 package Vistas;
 
+import AccesoADatos.AccesoProducto;
+import Entidades.Producto;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -34,11 +39,11 @@ public class masComprado extends javax.swing.JInternalFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        jDateChooser1 = new com.toedter.calendar.JDateChooser();
-        jDateChooser2 = new com.toedter.calendar.JDateChooser();
+        fecha1 = new com.toedter.calendar.JDateChooser();
+        fecha2 = new com.toedter.calendar.JDateChooser();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
-        jLabel4 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
 
         setClosable(true);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -54,8 +59,8 @@ public class masComprado extends javax.swing.JInternalFrame {
         jLabel3.setFont(new java.awt.Font("Arial", 0, 16)); // NOI18N
         jLabel3.setText("Ingrese el rango de fechas:");
         getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 220, -1, -1));
-        getContentPane().add(jDateChooser1, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 270, 150, 30));
-        getContentPane().add(jDateChooser2, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 270, 160, 30));
+        getContentPane().add(fecha1, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 270, 150, 30));
+        getContentPane().add(fecha2, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 270, 160, 30));
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -72,21 +77,53 @@ public class masComprado extends javax.swing.JInternalFrame {
 
         getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 340, 440, 100));
 
-        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/1.png"))); // NOI18N
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 790, 570));
+        jButton2.setText("Buscar");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(630, 270, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        borrarFilaTabla();
+        Producto p = new Producto();
+        AccesoProducto acc = new AccesoProducto();
+        
+        LocalDate F1 = fecha1.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        LocalDate F2 = fecha2.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        
+        
+        
+         
+        List<Producto> lista=acc.masComprados(F1, F2);
+        for (Producto a: lista){
+           model.addRow(new Object[]{a.getNombre(),a.getStock()});
+        }
+    }//GEN-LAST:event_jButton2ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private com.toedter.calendar.JDateChooser jDateChooser1;
-    private com.toedter.calendar.JDateChooser jDateChooser2;
+    private com.toedter.calendar.JDateChooser fecha1;
+    private com.toedter.calendar.JDateChooser fecha2;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
+
+private void borrarFilaTabla(){
+        int indice = model.getRowCount() -1;
+        
+        for(int i = indice;i>=0;i--){
+            model.removeRow(i); 
+        }
+    }
+    
 }
